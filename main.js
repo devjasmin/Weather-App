776676c381f2149cf3a825e8c4350c2b512051c9
Jasmin <j.longa@gmx.ch>
2026-04-07 19:41:59 +0200

    wieder neu.

--- main.js
+++ main.js
@@ -1,22 +1,22 @@
 import "/styles/style.scss";
-import { getCurrentWeather } from "./fetching.js";
+import { getCurrentWeather } from "./API.js";
 // getCurrentWeather("New York"); // nur wenn ich diesen Ort angezeigt haben will.
 
 async function init() {
   const data = getCurrentWeather("Bern, Switzerland");
 
   const placeNameElement = document.querySelector("#place-name");
-  placeNameElement.textContent = (await data).location.name;
+  placeNameElement.textContent = data.location.name;
 
   const placeTemperatureElement = document.querySelector("#place-temperature");
-  placeTemperatureElement.textContent = (await data).current.temp_c + "°";
+  placeTemperatureElement.textContent = data.current.temp_c + "°";
 
   const placeConditionElement = document.querySelector("#place-condition");
-  placeConditionElement.textContent = (await data).current.condition.text;
+  placeConditionElement.textContent = data.current.condition.text;
   const maxMinTemperatureElement = document.querySelector(
     "#max-min-temperature",
   );
-  maxMinTemperatureElement.textContent = `H: ${(await data).forecast.forecastday[0].day.maxtemp_c}° / T: ${(await data).forecast.forecastday[0].day.mintemp_c}°`;
+  maxMinTemperatureElement.textContent = `H: ${data.forecast.forecastday[0].day.maxtemp_c}° / T: ${data.forecast.forecastday[0].day.mintemp_c}°`;
 }
 
 init();
