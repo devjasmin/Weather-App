4881b7c66897ee343950c5c58d7b44aea6d0daed
Jasmin <j.longa@gmx.ch>
2026-04-09 22:20:56 +0200

    LoadingSpinner läuft fast

--- src/main.js
+++ src/main.js
@@ -1,30 +1,31 @@
+import { showLoadingScreen, hideLoadingScreen } from "./loadingScreen.js";
 import "./styles/style.scss";
 import { formatTemperature } from "./utils.js";
-
 import { getCurrentWeather } from "./API.js";
 
 // getCurrentWeather("New York"); // nur wenn ich diesen Ort angezeigt haben will.
 
-async function init(location) {
-  const data = await getCurrentWeather((location = "Bern"));
+async function init() {
+  const data = await getCurrentWeather("Kloten");
 
   const placeNameElement = document.querySelector("#place-name");
-  placeNameElement.textContent = await data.location.name;
+  placeNameElement.textContent = data.location.name;
 
   const placeTemperatureElement = document.querySelector("#place-temperature");
   placeTemperatureElement.textContent =
     formatTemperature(data.current.temp_c) + "°";
 
   const placeConditionElement = document.querySelector("#place-condition");
-  placeConditionElement.textContent = await data.current.condition.text;
+  placeConditionElement.textContent = data.current.condition.text;
   const maxMinTemperatureElement = document.querySelector(
     "#max-min-temperature",
   );
   maxMinTemperatureElement.textContent = `H: ${formatTemperature(data.forecast.forecastday[0].day.maxtemp_c)}° / T: ${formatTemperature(data.forecast.forecastday[0].day.mintemp_c)}°`;
 }
 
-formatTemperature();
+showLoadingScreen();
 init();
+hideLoadingScreen();
 
 const Btnback = document.getElementById("weather-app__return-btn");
 Btnback.addEventListener("click", () => {
