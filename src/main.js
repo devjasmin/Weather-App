103758d2b3b71be81e33db553bf38866b2805ac2
Jasmin <j.longa@gmx.ch>
2026-04-09 20:16:05 +0200

    eingegebener Ort im JS wird korrekt angenommen

--- src/main.js
+++ src/main.js
@@ -1,23 +1,24 @@
 import "./styles/style.scss";
 
 import { getCurrentWeather } from "./API.js";
+
 // getCurrentWeather("New York"); // nur wenn ich diesen Ort angezeigt haben will.
 
-async function init() {
-  const data = await getCurrentWeather("Neuenhof, Switzerland");
+async function init(location) {
+  const data = await getCurrentWeather((location = "Bern"));
 
   const placeNameElement = document.querySelector("#place-name");
-  placeNameElement.textContent = data.location.name;
+  placeNameElement.textContent = await data.location.name;
 
   const placeTemperatureElement = document.querySelector("#place-temperature");
-  placeTemperatureElement.textContent = data.current.temp_c + "°";
+  placeTemperatureElement.textContent = (await data.current.temp_c) + "°";
 
   const placeConditionElement = document.querySelector("#place-condition");
-  placeConditionElement.textContent = data.current.condition.text;
+  placeConditionElement.textContent = await data.current.condition.text;
   const maxMinTemperatureElement = document.querySelector(
     "#max-min-temperature",
   );
-  maxMinTemperatureElement.textContent = `H: ${data.forecast.forecastday[0].day.maxtemp_c}° / T: ${data.forecast.forecastday[0].day.mintemp_c}°`;
+  maxMinTemperatureElement.textContent = `H: ${await data.forecast.forecastday[0].day.maxtemp_c}° / T: ${await data.forecast.forecastday[0].day.mintemp_c}°`;
 }
 
 init();
