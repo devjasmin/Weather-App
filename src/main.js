a260fe311f043e4dcbc3d2020087312886f4b19f
Jasmin <j.longa@gmx.ch>
2026-04-09 20:38:34 +0200

    Gradzahlen werden als ganze Zahl angezeigt.

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import "./styles/style.scss";
+import { formatTemperature } from "./utils.js";
 
 import { getCurrentWeather } from "./API.js";
 
@@ -11,16 +12,18 @@ async function init(location) {
   placeNameElement.textContent = await data.location.name;
 
   const placeTemperatureElement = document.querySelector("#place-temperature");
-  placeTemperatureElement.textContent = (await data.current.temp_c) + "°";
+  placeTemperatureElement.textContent =
+    (await formatTemperature(data.current.temp_c)) + "°";
 
   const placeConditionElement = document.querySelector("#place-condition");
   placeConditionElement.textContent = await data.current.condition.text;
   const maxMinTemperatureElement = document.querySelector(
     "#max-min-temperature",
   );
-  maxMinTemperatureElement.textContent = `H: ${await data.forecast.forecastday[0].day.maxtemp_c}° / T: ${await data.forecast.forecastday[0].day.mintemp_c}°`;
+  maxMinTemperatureElement.textContent = `H: ${await formatTemperature(data.forecast.forecastday[0].day.maxtemp_c)}° / T: ${await formatTemperature(data.forecast.forecastday[0].day.mintemp_c)}°`;
 }
 
+formatTemperature();
 init();
 
 const Btnback = document.getElementById("weather-app__return-btn");
