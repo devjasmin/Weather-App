88ecaa496afc00ff5dacf09a9857c0f5d3f98e51
Jasmin <j.longa@gmx.ch>
2026-04-11 19:57:21 +0200

    neues config

--- src/loadingScreen.js
+++ src/loadingScreen.js
@@ -1,8 +1,10 @@
 export function showLoadingScreen(location) {
   const screen = document.getElementById("loading-screen");
   const message = document.getElementById("loadingScreen__message");
+  const spinner = document.getElementById("loadingScreen__spinner");
   screen.style.display = "flex";
   message.textContent = `Die Wetterdaten werden für ${location} geladen...`;
+  spinner.style.display = "flex";
 }
 
 export function hideLoadingScreen() {
