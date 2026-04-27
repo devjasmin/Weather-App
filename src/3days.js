50344f76ba3d027036f814603325d2632e257e03
Jasmin <j.longa@gmx.ch>
2026-04-27 17:41:52 +0200

    3 Tages Ansicht angepasst

--- src/3days.js
+++ src/3days.js
@@ -16,13 +16,23 @@ export function getDays(forecastday) {
               });
 
       return `      
-                <div class="forecast-days__item">
-               
-                <p class="forecast-days__label-day">${label}
-                <img src="https:${days.day.condition.icon}" alt="Bild">    H: ${formatTemperature(days.day.maxtemp_c)}°     T: ${formatTemperature(days.day.mintemp_c)}°     Wind bis zu ${days.day.maxwind_kph} Km/h
-                  </p>
-                   </div>
-                `;
+              <div class="forecast-days__item"> 
+      <span class="forecast-days__label-day"> ${label}</span>
+
+      <img class="forecast-days__icon" src="https:${days.day.condition.icon}" alt="Bild">
+
+      <span class="forecast-days__max_temp">
+        H: ${formatTemperature(days.day.maxtemp_c)}° 
+        </span>
+
+        <span class="forecast-days__min_temp">
+        T: ${formatTemperature(days.day.mintemp_c)}° </span>
+      
+        <span class="forecast-days__wind">
+            Wind: ${days.day.maxwind_kph} km/h
+          </span>
+        </div>
+      `;
     })
     .join("");
 
