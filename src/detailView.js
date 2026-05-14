d092218d52eeee097f00af26b797ff19c499b200
Jasmin <j.longa@gmx.ch>
2026-05-14 13:32:35 +0200

    “Back” button added and styled

--- src/detailView.js
+++ src/detailView.js
@@ -8,6 +8,7 @@ import { getMiniStats } from "./mini-stats.js";
 import { formatClock } from "./utils.js";
 import { getConditionImagePath } from "./conditions.js";
 import { loadMenu } from "./menu.js";
+//import { addFavorite } from "./state.js";
 
 export async function loadDetailView(cityName) {
   renderLoadingScreen("Die Wetterdaten werden für " + cityName + " geladen...");
@@ -31,6 +32,7 @@ function renderDetailView(weatherData) {
   }
 
   rootElement.innerHTML =
+    getActionsBarHTML() +
     getHeaderHTML(
       location.name,
       formatTemperature(current.temp_c) + "°",
@@ -48,22 +50,36 @@ function renderDetailView(weatherData) {
     `<div class ="mini-stats" glass-background>
     ${getMiniStats(forecast.forecastday, current)}`;
 
-  const Btnback = document.querySelector(".weather-app__return-btn");
-  if (Btnback) {
-    Btnback.addEventListener("click", () => {
+  const backBtn = document.querySelector(".weather-app__return-btn");
+  if (backBtn) {
+    backBtn.addEventListener("click", () => {
       rootElement.classList.remove("show-background");
       rootElement.style = "";
       loadMenu("Oslo");
     });
   }
+  function getActionsBarHTML() {
+    const backIcon = `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 19.5 8.25 12l7.5-7.5" />
+</svg>
+`;
+
+    const favoriteIcon = `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M11.48 3.499a.562.562 0 0 1 1.04 0l2.125 5.111a.563.563 0 0 0 .475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 0 0-.182.557l1.285 5.385a.562.562 0 0 1-.84.61l-4.725-2.885a.562.562 0 0 0-.586 0L6.982 20.54a.562.562 0 0 1-.84-.61l1.285-5.386a.562.562 0 0 0-.182-.557l-4.204-3.602a.562.562 0 0 1 .321-.988l5.518-.442a.563.563 0 0 0 .475-.345L11.48 3.5Z" />
+</svg>
+`;
+
+    return `
+          <div class="weather-app__header">
+          <div class="weather-app__return-btn">${backIcon}</div>
+          <div class="weather-app__favorite-btn">${favoriteIcon}</div> 
+          </div>  
+  `;
+  }
 }
 
 function getHeaderHTML(location, currentTemp, condition, maxTemp, minTemp) {
   return `
-      <div class="weather-app__header">
-      <button class="weather-app__return-btn"><svg width="26" height="26" viewBox="0 0 26 26" fill="none" xmlns="http://www.w3.org/2000/svg">
-  <path d="M15 18L9 12L15 6" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-</svg></button></div>
         <div class="weather-app__main">
           <div class="weather-app__place-name" >${location}</div>
           <div class="weather-app__place-temperature" >${currentTemp}</div>
